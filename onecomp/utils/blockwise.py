7480402b086e1f34acd47daa1e0ea558103448d1
aki916 <aki916.genyo@gmail.com>
2026-07-21 11:07:33 +0900

    fix unfinished merge

--- onecomp/utils/blockwise.py
+++ onecomp/utils/blockwise.py
@@ -307,10 +307,6 @@ def _compute_per_type_attention_masks(blocks_parent, kwargs, unique_layer_types)
     Uses create_causal_mask / create_sliding_window_causal_mask
     from transformers to produce the correct mask per layer type.
     """
-<<<<<<< HEAD
-
-=======
->>>>>>> feature/qwen36_27b_step2
     from transformers.masking_utils import (
         create_causal_mask,
         create_sliding_window_causal_mask,
